a3bc15282d04a185cd1fd9b573e967dde5cc08ed
Gibran García <a385898@uach.mx>
2025-10-15 07:55:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import math
 
 st.title("Aplicación Para Calcular Figuras y Relaciones Trigonométricas")
 
+# Selección de figura
+figura = st.selectbox("Selecciona una figura geométrica",["Circulo"])
+
 # Widget para ingresar el radio
 radio = st.slider ("Selecciona el radio", 0.0, 10.0, 5.0)
 
@@ -12,4 +15,7 @@ area = math.pi * radio**2
 
 # Mostrar resultado
 st.write(f"El área del círculo con radio {radio} es: {area: .2f}")
+
+# Calculo del perímetro de circulo
+perimetro = 2 * math.pi * radio 
 
